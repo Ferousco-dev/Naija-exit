78055ad98968586700e421bdd8a63c58347ad695
feranmi <feranmioresajo@gmail.com>
2026-04-27 15:33:54 +0100

    fix cloud run deployment

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distPath = path.join(__dirname, "dist");
+
+app.use(express.static(distPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Running on", PORT);
+  console.log("Server running on", PORT);
 });
