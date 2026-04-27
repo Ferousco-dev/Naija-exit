b189a55292acc6c01c1ce424cc3e1c8960af96bd
feranmi <feranmioresajo@gmail.com>
2026-04-27 16:12:21 +0100

    fix: working server for cloud run

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const distPath = path.join(__dirname, "dist");
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.use(express.static(distPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(distPath, "index.html"));
+// SAFE fallback route
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
